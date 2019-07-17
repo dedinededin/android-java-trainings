1a35e605ef4532bd64559ab274d09cfc44458443
dedinededin <ibrahimmdogann@gmail.com>
2019-07-17 15:43:55 +0300

    Login Screen implementation

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,27 +1,45 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static Button button_second;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        onClickButtonListener();
     }
 
     public void onButtonClick(View v) {
-        EditText e1 = (EditText) findViewById(R.id.editText);
-        EditText e2 = (EditText) findViewById(R.id.editText2);
-        EditText e3 = (EditText) findViewById(R.id.editText3);
+        EditText e1 = findViewById(R.id.editText);
+        EditText e2 = findViewById(R.id.editText2);
+        TextView e3 = findViewById(R.id.editText3);
 
         int num1 = Integer.parseInt(e1.getText().toString());
         int num2 = Integer.parseInt(e2.getText().toString());
 
         int sum = num1 + num2;
         e3.setText(Integer.toString(sum));
+    }
+
+    public void onClickButtonListener() {
+        button_second = findViewById(R.id.button2);
+        button_second.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(".SecondActivity");
+                        startActivity(intent);
+                    }
+                }
+        );
     }
 }
---
+++ app/src/main/java/com/example/myapplication/SecondActivity.java
@@ -0,0 +1,79 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private static EditText usernameEditText;
+    private static EditText passwordEditText;
+    private static Button loginButton;
+    private static TextView result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        loginButtonListener();
+    }
+
+    public void loginButtonListener() {
+        usernameEditText = findViewById(R.id.usernameEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+
+        loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                sendRequest();
+
+
+                if (usernameEditText.getText().toString().equals("admin") &&
+                        passwordEditText.getText().toString().equals("password")
+                ) {
+                    Toast.makeText(getApplicationContext(), "You successfully logged in.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private void sendRequest() {
+        result = findViewById(R.id.result);
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://www.google.com";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+
+                        result.setText("Response is: " + response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                result.setText("That didn't work!");
+            }
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+}
